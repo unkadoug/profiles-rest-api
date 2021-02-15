50090ce3516f9de6024df634966cf170faf9006f
Doug Moore <dougm@rice.edu>
2021-02-15 00:57:57 -0600

    Added login API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from profiles_api import models
 
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
